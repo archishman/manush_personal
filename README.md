8bbcae66fb84de672f2d6c20b9e6fec1af1aeffb
tck35 <tck35@cornell.edu>
2020-11-03 21:49:39 -0500

    Can interact with Manush via any chess GUI

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # manush
 Manush - A Human-like Chess bot
+
+Currnent Progress:
+
+Manush plays random moves. Load manush.exe into your chess GUI of choice to 
+interact with the engine.
---
+++ manush.py
@@ -0,0 +1,14 @@
+"""
+Contains the main function used to run the engine
+"""
+
+import uci
+
+
+"""
+[run_manush()] starts and runs the engine.
+"""
+def run_manush():
+    uci.uci_communication()
+
+run_manush()
